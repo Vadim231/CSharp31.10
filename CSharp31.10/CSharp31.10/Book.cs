a97ca5886f623300e17a49bddf739ac07711a2be
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2018-11-13 00:09:36 +0300

    Add files via upload

---
+++ CSharp31.10/CSharp31.10/Book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharp31._10
+{
+    public enum Count
+    {
+        one=1,
+        two,
+        three
+    }
+
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Book : Attribute
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string name, int cost, string author, int year)
+        {
+            Name = name;
+            Cost = cost;
+            Author = author;
+            Year = year;
+        }
+    }
+  
+
+    internal class BookCountAttribute : Attribute
+    {
+        protected Count count;
+        public Count Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        public BookCountAttribute(Count count)
+        {
+            this.count = count;
+        }
+    }
+
+    class BookCountTestClass
+    {
+        [BookCount(Count.one)]
+        public void OneMethod() { Console.WriteLine("Одна книга"); }
+
+        [BookCount(Count.two)]
+        public void TwoMethod() { Console.WriteLine("Две книги"); }
+
+        [BookCount(Count.three)]
+        public void ThreeMethod() { Console.WriteLine("Три книги"); }
+    }
+}
---
+++ CSharp31.10/CSharp31.10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp31._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("qwe", 2400, "Vadim", 2000);
+            Book book2 = new Book("asd", 11230, "Vamid", 1999);
+            Book book3 = new Book("zxc", 15740, "Admin", 2001);
+            Book book4 = new Book("123", 12200, "Adnim", 2010);
+            BookCountTestClass book5 = new BookCountTestClass();
+            book5.OneMethod();
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            using (FileStream fs = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, book1);
+                formatter.Serialize(fs, book2);
+                formatter.Serialize(fs, book3);
+                formatter.Serialize(fs, book4);
+                Console.WriteLine("Объект сериализован");
+            }
+            
+            using (FileStream fileStream = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                Book book = (Book)formatter.Deserialize(fileStream);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine("Название: " + book.Name + " Цена: " + book.Cost + " Автор: " + book.Author + " Год издания: " + book.Year);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
